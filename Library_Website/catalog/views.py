2f883ca348cd86efd7cbcbdb629c36f7b04df43f
Shashwat Singh <shashwatsingh741@gmail.com>
2022-11-04 13:35:40 +0530

    Added homepage view logic and template

--- Library_Website/catalog/views.py
+++ Library_Website/catalog/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Author, Genre, Language, Book, BookInstance
 
 # Create your views here.
 def index(request):
-    return HttpResponse('hello')
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_avail = BookInstance.objects.filter(status___exact='a').count()
+
+    context = {
+        'num_books' : num_books,
+        'num_instances' : num_instances,
+        'num_instances_avail' : num_instances_avail
+    }
+
+    return render(request, '/catalog/index.html', context=context)
