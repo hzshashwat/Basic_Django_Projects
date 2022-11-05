5814031e0d5b786e7285118f7ac34c0adc7bd7ec
Shashwat Singh <shashwatsingh741@gmail.com>
2022-11-06 01:43:09 +0530

    Added view logic and routing for User Specific Pages

--- Library_Website/catalog/urls.py
+++ Library_Website/catalog/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('create_book/', views.BookCreate.as_view(), name= 'create_book'),
     path('book/<int:pk>/', views.BookDetail.as_view(), name = 'book_detail'),
     path('my_view/', views.my_view, name = 'myview'),
-    path('signup', views.SignupView.as_view(), name = 'signup')
+    path('signup', views.SignupView.as_view(), name = 'signup'),
+    path('profile/', views.CheckedOutBooksByUser.as_view(), name = 'profile')
 ]
