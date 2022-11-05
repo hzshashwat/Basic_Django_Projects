7fc8a564d81be9a79c2dc8a7dc1ee24b22b09bce
Shashwat Singh <shashwatsingh741@gmail.com>
2022-11-05 16:30:59 +0530

    Setup SignUp Page template and logic

--- Library_Website/catalog/urls.py
+++ Library_Website/catalog/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.index, name ='index'),
     path('create_book/', views.BookCreate.as_view(), name= 'create_book'),
     path('book/<int:pk>/', views.BookDetail.as_view(), name = 'book_detail'),
-    path('my_view/', views.my_view, name = 'myview')
+    path('my_view/', views.my_view, name = 'myview'),
+    path('signup', views.SignupView.as_view(), name = 'signup')
 ]
--- Library_Website/catalog/views.py
+++ Library_Website/catalog/views.py
@@ -3,6 +3,8 @@ from .models import Author, Genre, Language, Book, BookInstance
 from django.views.generic import CreateView, DetailView
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
 
 # Create your views here.
 def index(request):
@@ -28,3 +30,8 @@ class BookDetail(DetailView):
 @login_required
 def my_view(request):
     return render(request, 'catalog/my_view.html')
+
+class SignupView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'catalog/signup.html'
